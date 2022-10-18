6cd4dc84f850bd07c3e9ebe146f0a1a3387b3fff
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-10-17 17:53:19 -0700

    replace

--- _putchar.c
+++ _putchar.c
@@ -1,132 +1,38 @@
 #include "main.h"
 
 /**
- * print_char - prints character
- * @ap: argument pointer
- * @params: the parameters struct
+ * _puts - prints a string with newline
+ * @str: the string to print
  *
- * Return: number chars printed
+ * Return:( str-a)
  */
-int print_char(va_list ap, params_t *params)
+int _puts(char *str)
 {
-	char pad_char = ' ';
-	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
+	char *a = str;/*declaration of variables*/
 
-	if (params->minus_flag)
-		sum += _putchar(ch);
-	while (pad++ < params->width)
-		sum += _putchar(pad_char);
-	if (!params->minus_flag)
-		sum += _putchar(ch);
-	return (sum);
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
 }
 
 /**
- * print_int - prints integer
- * @ap: argument pointer
- * @params: the parameters struct
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: number chars printed
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
-int print_int(va_list ap, params_t *params)
+int _putchar(int c)
 {
-	long l;
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
 
-	if (params->l_modifier)
-		l = va_arg(ap, long);
-	else if (params->h_modifier)
-		l = (short int)va_arg(ap, int);
-	else
-		l = (int)va_arg(ap, int);
-	return (print_number(convert(l, 10, 0, params), params));
-}
-
-/**
- * print_string - prints string
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
- */
-int print_string(va_list ap, params_t *params)
-{
-	char *str = va_arg(ap, char *), pad_char = ' ';
-	unsigned int pad = 0, sum = 0, i = 0, j;
-
-	(void)params;
-	switch ((int)(!str))
-		case 1:
-			str = NULL_STRING;
-
-	j = pad = _strlen(str);
-	if (params->precision < pad)
-		j = pad = params->precision;
-
-	if (params->minus_flag)
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
+		write(1, buf, i);
+		i = 0;
 	}
-	while (j++ < params->width)
-		sum += _putchar(pad_char);
-	if (!params->minus_flag)
-	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
-	}
-	return (sum);
-}
-
-/**
- * print_percent - prints string
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
- */
-int print_percent(va_list ap, params_t *params)
-{
-	(void)ap;
-	(void)params;
-	return (_putchar('%'));
-}
-
-/**
- * print_S - custom format specifier
- * @ap: argument pointer
- * @params: the parameters struct
- *
- * Return: number chars printed
- */
-int print_S(va_list ap, params_t *params)
-{
-	char *str = va_arg(ap, char *);
-	char *hex;
-	int sum = 0;
-
-	if ((int)(!str))
-		return (_puts(NULL_STRING));
-	for (; *str; str++)
-	{
-		if ((*str > 0 && *str < 32) || *str >= 127)
-		{
-			sum += _putchar('\\');
-			sum += _putchar('x');
-			hex = convert(*str, 16, 0, params);
-			if (!hex[1])
-				sum += _putchar('0');
-			sum += _puts(hex);
-		}
-		else
-		{
-			sum += _putchar(*str);
-		}
-	}
-	return (sum);
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
